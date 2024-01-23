6eca9fa420e6246316c3d2ea5d9804dfabdc5e5e
amu94777 <amu94777@gmail.com>
2024-01-23 06:45:49 +0530

    vpc

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "rvpc" {
   )
 }
 ###### internet gate way ####
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.rvpc.id
  tags = merge(var.comman_tags,var.gw_tags,
  {
@@ -43,7 +43,7 @@ resource "aws_subnet" "private" {
   )
 }
 ####### database subnet conf ####
-resource "aws_subnet" "databse" {
+resource "aws_subnet" "database" {
   count = length(var.database_subnets_cidr)
   vpc_id     = aws_vpc.rvpc.id
   cidr_block = var.database_subnets_cidr[count.index]
@@ -55,4 +55,84 @@ resource "aws_subnet" "databse" {
             }
   )
 }
+### eip conf ####
+resource "aws_eip" "eip" {
+  domain = "vpc"
+} 
+
+####natgateway conf ####
+resource "aws_nat_gateway" "rnatgate" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(var.comman_tags,var.nat_gateway_tags,
+         
+  {
+    Name = "${local.name}"
+  }
+  )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
+###### public route table conf ######
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.rvpc.id
+  tags = merge(var.comman_tags,var.public_route_table_tags,
+         {
+           Name = "${local.name}-public"
+  }
+  )
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.rvpc.id
+  tags = merge(var.comman_tags,var.private_route_table_tags,
+         {
+           Name = "${local.name}-private"
+  }
+  )
+}
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.rvpc.id
+  tags = merge(var.comman_tags,var.database_route_table_tags,
+         {
+           Name = "${local.name}-database"
+  }
+  )
+}
+##### adding routes ####
+
+resource "aws_route" "public_route" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+resource "aws_route" "private_route" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.rnatgate.id
+}
+resource "aws_route" "database_route" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.rnatgate.id
+}
+####### route table assosiation with subnets ####
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets_cidr)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets_cidr)
+  subnet_id      = element(aws_subnet.private[*].id, count.index)
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnets_cidr)
+  subnet_id      = element(aws_subnet.database[*].id, count.index)
+  route_table_id = aws_route_table.database.id
+}
+
 
