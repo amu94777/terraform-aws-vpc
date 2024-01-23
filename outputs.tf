ec343c9a7a00645a8ce5c0fe180600319f51abf8
amu94777 <amu94777@gmail.com>
2024-01-23 11:28:45 +0530

    vpc

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,15 @@
 output "azs" {
     value = data.aws_availability_zones.azs.names
-  
+}
+output "rvpc_id" {
+    value = aws_vpc.rvpc.id
+}
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
 }
